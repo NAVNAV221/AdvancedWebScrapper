cec2deb0b320c101bfb79aee04f4856c84bafcac
NAVNAV221 <52596137+NAVNAV221@users.noreply.github.com>
2021-11-29 22:09:17 +0200

    Update cralwer.py

--- cralwer.py
+++ cralwer.py
@@ -1,8 +1,8 @@
 import time
-start_time = time.time()
 from website import Website
 
 if __name__ == '__main__':
+    start_time = time.time()
     page = Website('https://red574890.github.io/colors.github.io/green.html', 'page')
     page_tree = page.crawl_url_by_depth(2)
 
